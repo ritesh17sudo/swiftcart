aacdbc08ad44b074d4c429c9413a3ba8af590f3d
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-09-24 18:35:09 +0530

    Server corrected

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from "dotenv";
 import morgan from 'morgan';
-import connectDB from './controller/config/db.js';
+// import connectDB from './controller/config/db.js';
 import authRoutes from './routes/authRoute.js';
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import connectDB from './config/db.js'; // Corrected path
+
 
 // Configure env
 dotenv.config()
